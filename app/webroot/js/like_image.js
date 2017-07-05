0e984d0bb43ab308b462f4bbdc7e5573e30543bb
TeamC <teamc@TMHs-MacBook-2.local>
2017-07-05 14:37:37 +0700

    Add js like image

---
+++ app/webroot/js/like_image.js
@@ -0,0 +1,35 @@
+function like(id) {
+
+    id_article = id;
+    $.ajax({
+        url: '/STOCK_PHOTO_teamC/images/like',
+        type: "POST",
+        data: { id_article : id_article},
+        dataType : 'json',
+       
+        success: function(data) {
+            console.log(data);
+            if (data.is_like == true) {
+                $('button#'+id).removeClass('btn-default');
+                $('button#'+id).addClass('btn-danger');
+            }
+            else {
+                $('button#'+id).removeClass('btn-danger');
+                $('button#'+id).addClass('btn-default');
+            }
+            $('#'+id+'.like_count').empty();
+            $('#'+id+'.like_count').text(data.like_count);
+
+        },
+        error: function( xhr, status, errorThrown ) {
+            if (errorThrown == 'Forbidden') {
+                var url = "/STOCK_PHOTO_teamC/users/login";
+                $(location).attr('href',url);
+            }
+        },
+
+    });
+}  
+
+
+    
